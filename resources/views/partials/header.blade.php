33708461d10d85b9a9975a3bbad0a3faff4a8ba3
Mark-dev30 <fronterremarco@gmail.com>
2023-02-16 18:54:09 +0100

    added img, header to complete, main completed

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,26 @@
+<header>
+    <div class="container cnt-header">
+        <div class="row row-header">
+            <div class="col-2">
+                <div class="logo">
+                    <img class="image-logo" src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="DC-logo">
+                </div>
+            </div>
+            <div class="col-10">
+                <div class="menu-bar">
+                    <ul>
+                        @foreach ($menu as $key => $item )
+                        <li>
+                            <a class="{{ Route::currentRouteName() == $key ? 'active' : ''}}" href="#">{{ $key }}</a>
+                        </li>
+                        @endforeach
+                        <li class="li-input">
+                            <input type="text" placeholder="search">
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+</header>
